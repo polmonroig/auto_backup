ba9b0cca707353ed9ce7c39cdeb4e382a5d5d0cc
polmonroig <polmonroig@yahoo.es>
2020-06-21 11:22:30 +0200

    Add scripting mode

--- auto_backup.py
+++ auto_backup.py
@@ -3,8 +3,9 @@ from version import __version__
 from parser import Parser
 from database import ProjectDatabase
 import os
+import sys
 
-def init(database):
+def interactive(database):
     parser = Parser()
     exe = os.path.dirname(os.path.realpath(__file__))
     exe = exe.split('/')
@@ -19,10 +20,19 @@ def init(database):
         database.interact(action)
 
 
+def scripting(database):
+    path = sys.argv[1]
+    parser = Parser()
+    for action in parser.read_file(path):
+        database.interact(action)
+
 def main():
     print('Backup utility', __version__)
     database = ProjectDatabase()
-    init(database)
+    if len(sys.argv) >= 2:
+        scripting(database)
+    else:
+        interactive(database)
 
 
 if __name__ == '__main__':
